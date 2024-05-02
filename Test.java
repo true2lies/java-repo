e9c09b5ee3704c41f996095a62f46a6116b7817c
true2lies <162881360+true2lies@users.noreply.github.com>
2024-05-02 13:29:37 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("**************************************");
 		System.out.println("Hello from Github");
+		System.out.println("This is a Test");
 		System.out.println("**************************************");
 	}
 
