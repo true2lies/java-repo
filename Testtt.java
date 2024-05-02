3a793fa9a214e60b8b54c26e76e4da8ff713ad29
true2lies <162881360+true2lies@users.noreply.github.com>
2024-05-02 14:34:08 +0100

    Create Testtt.java

---
+++ Testtt.java
@@ -0,0 +1,10 @@
+public class Testtt {
+
+	public static void main(String[] args) {
+		System.out.println("**************************************");
+		System.out.println("Hello from Github");
+		System.out.println("This is a Testttdre");
+		System.out.println("**************************************");
+	}
+
+}
