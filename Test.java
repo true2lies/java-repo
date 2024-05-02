dfc61f22704916e24d79e39810f517311729bb38
true2lies <162881360+true2lies@users.noreply.github.com>
2024-05-02 10:09:58 +0100

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("**************************************");
-		System.out.println("Hello from Github");
-		System.out.println("**************************************");
-	}
-
-}
