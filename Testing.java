a492c051641ce4b4e3c8126bb48c2fcc333ece22
true2lies <162881360+true2lies@users.noreply.github.com>
2024-05-02 14:50:09 +0100

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,11 @@
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("**************************************");
+		System.out.println("Hello from Github");
+		System.out.println("This is a Testttdre");
+		System.out.println("This is a Testttdre");
+		System.out.println("**************************************");
+	}
+
+}
